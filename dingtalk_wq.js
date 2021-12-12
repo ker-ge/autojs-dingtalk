1d6cbb05dcc4c6d587a517e8de0a543d95dedef4
ker <ker>
2021-12-12 11:46:27 +0800

    修改休假不打卡，调用接口

--- dingtalk_wq.js
+++ dingtalk_wq.js
@@ -9,10 +9,12 @@ let config = {
   "workTitle": "工作台",
   "stopDingtalk": "强行停止", // 关闭钉钉后台运行、不同手机不一样的按钮、强行停止、结束运行、关闭
   "randomMin": 0, // 随机最小数
-  "randomMax": 10, // 随机最大数
+  "randomMax": 5, // 随机最大数
+  "apiUrl": "http://apis.juhe.cn/fapig/calendar/day.php",
+  "apiKey": "",
 };
 
-let keepScreenOnMinutes = 5; // 保存常亮时间
+let keepScreenOnMinutes = 15; // 保存常亮时间
 let sleepTime = 5 * 1000; // 暂停的时间
 let sleepTime_ding = 10 * 1000; // 钉钉暂停的时间
 
@@ -20,8 +22,6 @@ let $$init = {
   start() {
     // 判断是否是放假
     isHoliday();
-    // 随机延迟
-    randomSleep();
 
     return wakeUp();
 
@@ -40,7 +40,7 @@ let $$init = {
       }
       //注册退出事件
       events.on("exit", function () {
-        console.log('已经全部处理完毕,回到桌面');
+        toastLog('已经全部处理完毕,回到桌面');
         device.keepScreenOn(5000); // 脚本执行结束,常亮5秒钟、5秒后按系统自动设定来自己锁屏
         home(); // 回到桌面
       });
@@ -64,25 +64,40 @@ let $$init = {
 
     // 判断是否是放假
     function isHoliday() {
-      let nowday = NowTime.getDay(); // 0-周日，6-周六
-      if (nowday == 0 || nowday == 6) {
-        exit(); // 周末直接停止脚本
+      // let nowday = NowTime.getDay(); // 0-周日，6-周六
+      // if (nowday == 0 || nowday == 6) {
+      //   exit(); // 周末直接停止脚本
+      // }
+      let year = NowTime.getFullYear();
+      let month = NowTime.getMonth() + 1;
+      let day = NowTime.getDate();
+      day = day > 9 ? day : '0' + day;
+      let dater = year + "-" + month + "-" + day;
+      let url = config.apiUrl + "?date=" + dater + "&key=" + config.apiKey;
+      let res = http.get(url);
+      // let data = res.body.json();
+      // console.log(data);
+      let statusDesc = res.body.json()['result']['statusDesc'];
+      if (statusDesc != '工作日') {
+        toastLog('非工作日，停止脚本');
+        engines.myEngine().forceStop(); // 非工作日直接停止脚本
       }
-      // 节假日需要用接口
     }
 
-    // 随机延迟
-    function randomSleep() {
+    // 获取随机睡觉的时间
+    function getRangSleep() {
+      // 进入钉钉就随机延迟
       let randNum = random(config.randomMin, config.randomMax);
       toastLog("随机延迟" + randNum + "分钟");
-      sleep(randNum * 60 * 100); // 随机睡觉的数字,分钟为单位
+      return randNum * 60 * 1000;
     }
 
     // 关闭钉钉（这个关闭操作根据自己设备具体情况配置）
     function closeDingtalk() {
       sleep(sleepTime);
-      toastLog("关闭钉钉APP ing");
+      toastLog("先关闭钉钉APP ing");
       let packageName = app.getPackageName("钉钉");
+      if (!packageName) return postMessage("没有找到可以打开的钉钉");
       app.openAppSetting(packageName);
       text("钉钉").waitFor();
       sleep(sleepTime);
@@ -96,14 +111,16 @@ let $$init = {
     // 打开钉钉
     function openDingtalk() {
       sleep(sleepTime);
-      toastLog("启动钉钉APP ing");
+      toastLog("在启动钉钉APP ing");
       let res = app.launchApp("钉钉");
       if (!res) return postMessage("没有找到可以打开的钉钉");
     }
 
     //进入考勤页面
     function inKaoqin() {
-      sleep(sleepTime_ding); // 有时候进入钉钉比较慢
+      let rangSleepTime = getRangSleep();
+      if (rangSleepTime < sleepTime_ding) rangSleepTime = sleepTime + sleepTime_ding; // 防止出现0的情况
+      sleep(rangSleepTime);
       workBtn = text('工作台').findOne(10000); // 找到工作台
       workBtn.parent().parent().click(); // 点击工作台
       sleep(sleepTime_ding);
